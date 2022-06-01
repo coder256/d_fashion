b462f560ee7ac12b20ed8c62e146927d797a1914
Andrew M <mamugasa@gmail.com>
2022-06-01 11:06:31 +0300

    ran migrations

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->enum('role', ['user','admin','data_entrant'])->default('user');
+            $table->integer('shirt_sleeve')->nullable();
+            $table->integer('chest')->nullable();
+            $table->integer('thigh')->nullable();
+            $table->integer('waist')->nullable();
+            $table->integer('trouser_length')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/migrations/2022_05_31_205029_create_cart_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart', function (Blueprint $table) {
+            $table->id();
+            $table->integer('product_id');
+            $table->integer('user_id');
+            $table->integer('quantity');
+            $table->integer('shirt_sleeve')->nullable();
+            $table->integer('chest')->nullable();
+            $table->integer('thigh')->nullable();
+            $table->integer('waist')->nullable();
+            $table->integer('trouser_length')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart');
+    }
+}
